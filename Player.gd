233a151fe71ae06b552ec3abb34e21d3413eb8ca
Edward Carmack <ariemethacc@gmail.com>
2021-02-25 18:32:27 -0500

    removed unused hit signal from player updated project setting to support touch.

--- Player.gd
+++ Player.gd
@@ -5,6 +5,8 @@ signal hit
 # Declare member variables here. Examples:
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+# Add this variable to hold the clicked position.
+var target = Vector2()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,18 +14,19 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y +=1
-	if Input.is_action_pressed(("ui_up")):
-		velocity.y -=1 
+	# Move towards the target and stop when close.
+	if position.distance_to(target) > 10:
+		velocity = target - position
+		
+		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
